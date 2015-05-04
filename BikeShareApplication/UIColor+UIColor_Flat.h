7c3c6b5f727cc051f51985fc52d37c84d396a48c
CraigStojsin <stojsin@outlook.com>
2015-05-04 14:24:49 -0400

    first commit

---
+++ BikeShareApplication/UIColor+UIColor_Flat.h
@@ -0,0 +1,13 @@
+//
+//  UIColor+UIColor_Flat.h
+//  BikeShareApplication
+//
+//  Created by Craig Stojsin on 2015-05-04.
+//  Copyright (c) 2015 CraigCode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (UIColor_Flat)
+
+@end
---
+++ BikeShareApplication/UIColor+UIColor_Flat.m
@@ -0,0 +1,13 @@
+//
+//  UIColor+UIColor_Flat.m
+//  BikeShareApplication
+//
+//  Created by Craig Stojsin on 2015-05-04.
+//  Copyright (c) 2015 CraigCode. All rights reserved.
+//
+
+#import "UIColor+UIColor_Flat.h"
+
+@implementation UIColor (UIColor_Flat)
+
+@end
